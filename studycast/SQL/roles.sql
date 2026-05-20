a0fa4c19621d9b6ee59d832787d04c38c527a957
youngaahn <aya70713@gmail.com>
2026-05-20 22:13:25 +0900

    JWT 의존성 추가 / SQL 수정

--- studycast/SQL/roles.sql
+++ studycast/SQL/roles.sql
@@ -1,13 +1,13 @@
 CREATE TABLE roles (
     role_code SERIAL PRIMARY KEY,
     user_uuid UUID NOT NULL,
-    role VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL DEFAULT 'ROLE_USER',
     
     CONSTRAINT fk_user FOREIGN KEY(user_uuid)
     REFERENCES users(user_uuid) ON DELETE CASCADE,
     CONSTRAINT uq_user_role UNIQUE (user_uuid, role) 
 );
 
-COMMENT ON COLUMN roles.rolde_code IS '권한 식별 번호';
+COMMENT ON COLUMN roles.role_code IS '권한 식별 번호';
 COMMENT ON COLUMN roles.user_uuid IS '회원 식별 번호(FK(users.user_uuid))';
 COMMENT ON COLUMN roles.role IS '권한 (ADMIN / USER)';
