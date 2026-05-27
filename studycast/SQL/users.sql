14a99a029c986a024f4485a495de5f98e1e269ab
MoaisMoa <mom.moom.mu@gmail.com>
2026-05-27 12:27:47 +0900

    2026-05-27 : users.sql 수정 from.희진

--- studycast/SQL/users.sql
+++ studycast/SQL/users.sql
@@ -1,29 +1,23 @@
--- Active: 1778135432146@@127.0.0.1@5432@studycast_db
+-- 1. 회원 정보 테이블
 CREATE TABLE IF NOT EXISTS users (
     user_uuid UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     user_email VARCHAR(255) NOT NULL UNIQUE,
     user_password VARCHAR(255) NOT NULL,
     user_name VARCHAR(255) NOT NULL,
+    user_gender VARCHAR(50) NOT NULL DEFAULT '선택안함' CHECK (user_gender IN ('남성', '여성', '선택안함')),
     user_profile_image VARCHAR(255),
-    user_gender VARCHAR(20),
-    user_birth_date DATE,
-    user_bio VARCHAR(20),
-    user_status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE',
+    user_status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE' CHECK (user_status IN ('ACTIVE', 'WITHDRAWN')),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP DEFAULT NULL
 );
-
--- 컬럼 주석
 COMMENT ON COLUMN users.user_uuid IS '회원 식별 번호';
 COMMENT ON COLUMN users.user_email IS '회원 이메일';
 COMMENT ON COLUMN users.user_password IS '회원 비밀번호';
 COMMENT ON COLUMN users.user_name IS '회원 이름';
+COMMENT ON COLUMN users.user_gender IS '회원 성별 (남성, 여성, 선택안함)';
 COMMENT ON COLUMN users.user_profile_image IS '회원 프로필 이미지 URL';
-COMMENT ON COLUMN users.user_gender IS '회원 성별';
-COMMENT ON COLUMN users.user_birth_date IS '회원 생년월일';
-COMMENT ON COLUMN users.user_bio IS '회원 한 줄 각오';
-COMMENT ON COLUMN users.user_status IS '회원 상태 (ACTIVE, INACTIVE)';
+COMMENT ON COLUMN users.user_status IS '회원 상태 (ACTIVE, WITHDRAWN)';
 COMMENT ON COLUMN users.created_at IS '회원 가입 일시';
 COMMENT ON COLUMN users.updated_at IS '회원 정보 수정 일시';
 COMMENT ON COLUMN users.deleted_at IS '회원 탈퇴 일시';
