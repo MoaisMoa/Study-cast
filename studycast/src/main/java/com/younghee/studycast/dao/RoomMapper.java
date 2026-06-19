fd2d36d7fc6c477466d90535442653e057661757
moa <mom.moom.mu@gmail.com>
2026-06-19 18:36:34 +0900

    2026-06-19 : 오류 코드 수정

--- studycast/src/main/java/com/younghee/studycast/dao/RoomMapper.java
+++ studycast/src/main/java/com/younghee/studycast/dao/RoomMapper.java
@@ -1,6 +1,7 @@
 package com.younghee.studycast.dao;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.apache.ibatis.annotations.Mapper;
--- studycast/src/main/java/com/younghee/studycast/dto/response/RoomSnapshotResponse.java
+++ studycast/src/main/java/com/younghee/studycast/dto/response/RoomSnapshotResponse.java
@@ -1,38 +1,44 @@
-package com.younghee.studycast.dto.response;
+// package com.younghee.studycast.dto.response;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@builder
-public class RoomSnapshotResponse {
-    private Long roomId;
-    private String title;
-    private int maxMenber;
-    private String notice;
-    private boolean isHost;
-    private List<MemberInfo> members;
-    private List<ChatMessageInfo> messages;
+// import java.util.List;
 
-    @Data
-    @builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class MemberInfo {
-        private String userUuid;
-        private String userName;
-        private String userEmail;
-        private String userProfileImage;
-        private boolean cameraStatus;
-        private boolean micStatus;
-        private boolean isHost;
-    }
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-    public static class ChatmessageInfo {
-        private Long chatNo;
-        private String userUuid;
-        private String userName;
-        private String userProfileImage;
-        private String message;
-        private String sentAt;
-    }
-}
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @builder
+// public class RoomSnapshotResponse {
+//     private Long roomId;
+//     private String title;
+//     private int maxMenber;
+//     private String notice;
+//     private boolean isHost;
+//     private List<MemberInfo> members;
+//     private List<ChatMessageInfo> messages;
+
+//     @Data
+//     @builder
+//     @NoArgsConstructor
+//     @AllArgsConstructor
+//     public static class MemberInfo {
+//         private String userUuid;
+//         private String userName;
+//         private String userEmail;
+//         private String userProfileImage;
+//         private boolean cameraStatus;
+//         private boolean micStatus;
+//         private boolean isHost;
+//     }
+
+//     public static class ChatmessageInfo {
+//         private Long chatNo;
+//         private String userUuid;
+//         private String userName;
+//         private String userProfileImage;
+//         private String message;
+//         private String sentAt;
+//     }
+// }
