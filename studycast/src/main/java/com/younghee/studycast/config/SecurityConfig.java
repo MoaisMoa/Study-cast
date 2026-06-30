44fa9d9b65fa79686465a861d69df4f27faf032d
youngaahn <aya70713@gmail.com>
2026-06-30 15:32:48 +0900

    260630 안영아 Securityconfig 수정

--- studycast/src/main/java/com/younghee/studycast/config/SecurityConfig.java
+++ studycast/src/main/java/com/younghee/studycast/config/SecurityConfig.java
@@ -138,7 +138,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://study-cast-ten.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
